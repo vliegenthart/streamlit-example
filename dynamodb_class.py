dc03bacaa4cf0fc6d6d9aba1db81dfaa82d1e975
Daniel <daniel.vliegenthart@tellimer.com>
2022-12-29 20:09:35 +0000

    feat: 🎸 implement initial dynamodb integration

---
+++ dynamodb_class.py
@@ -0,0 +1,160 @@
+# ##### User
+# - Main entity which represents a customer.
+# - On the primary index, indexed by the Cognito user ID.
+# - On GSI the record is indexed by the customer's email to make it easy to retrieve a record by email.
+# - On GSI2, for enterprise customers only, the record is indexed by the enterprise ID and sorted by enterprise user type & email.
+
+# ##### Dataset
+
+# - Represents a dataset of a user.
+# - Indexed on primary index by user ID and sorted by dataset ID which is a "ulid". Enables querying datasets of a user sorted by creation date.
+# - Indexed on GSI by Reap Document ID. Enables finding the dataset by Reap Document ID, when Reap notifies an update to a document via SNS.
+# - Sorted on LSI by dataset name. Enables querying datasets of a user sorted by dataset name.
+
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+
+ENV = "staging"
+TABLE_NAME = f"parsel-backend-{ENV}"
+
+# For motivation see https://github.com/awsdocs/aws-doc-sdk-examples/blob/main/python/example_code/dynamodb/GettingStarted/scenario_getting_started_movies.py
+class Items:
+    """Encapsulates an Amazon DynamoDB table of items data."""
+
+    def __init__(self):
+        """
+        :param dyn_resource: A Boto3 DynamoDB resource.
+        """
+        self.dyn_resource = boto3.resource("dynamodb", region_name="eu-west-1")
+        self.table = None
+        self.table_name = None
+
+        self.exists(TABLE_NAME)
+
+    def exists(self, table_name):
+        """
+        Determines whether a table exists. As a side effect, stores the table in
+        a member variable.
+        :param table_name: The name of the table to check.
+        :return: True when the table exists; otherwise, False.
+        """
+        try:
+            table = self.dyn_resource.Table(table_name)
+            table.load()
+            exists = True
+        except ClientError as err:
+            if err.response["Error"]["Code"] == "ResourceNotFoundException":
+                exists = False
+            else:
+                logger.error(
+                    "Couldn't check for existence of %s. Here's why: %s: %s",
+                    table_name,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+                raise
+        else:
+            self.table = table
+            self.table_name = table_name
+        return exists
+
+    def get_user(self, id):
+        """
+        TODO
+
+        :param id: User Id
+        :return: The user data
+        """
+        try:
+            response = self.table.get_item(Key={"pk": id, "sk": "user"})
+        except ClientError as err:
+            logger.error(
+                "Couldn't get user %s from table %s. Here's why: %s: %s",
+                id,
+                self.table.name,
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response
+
+    def query_enterprise_users(self, enterprise_id):
+        return self.query(Key("gsi2Pk").eq(enterprise_id), "gsi-2")
+
+    def query_user_by_email(self, email):
+        return self.query(Key("gsiPk").eq(f"user#{email}"), "gsi")["Items"][0]
+
+    def query_user_datasets(self, user_id):
+        return self.query(Key("pk").eq(user_id), None, False)["Items"]
+
+    def query(
+        self,
+        key_condition_expression,
+        gsi_name=None,
+        is_index=True,
+    ):
+        """
+        Queries the table.
+        """
+        try:
+            if is_index:
+
+                response = self.table.query(
+                    IndexName=f"{self.table_name}-{gsi_name}",
+                    KeyConditionExpression=key_condition_expression,
+                )
+            else:
+                response = self.table.query(
+                    KeyConditionExpression=key_condition_expression,
+                )
+
+        except ClientError as err:
+            logger.error(
+                err.response["Error"]["Code"],
+                err.response["Error"]["Message"],
+            )
+            raise
+        else:
+            return response
+
+
+import decimal
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+# if __name__ == "__main__":
+#     ENV = "prod"
+#     TABLE_NAME = f"parsel-backend-{ENV}"
+
+#     items_table = Items(boto3.resource("dynamodb", region_name="eu-west-1"))
+#     exists = items_table.exists(TABLE_NAME)
+#     # user_response = (
+#     #     dict(items_table.get_user("user#55e6af69-c4de-47cd-9da1-53fb8acb0815")),
+#     # )
+#     # print(user_response)
+#     # enterprise_users_response = items_table.query_enterprise_user_ids(
+#     #     user_response[0]["Item"]["gsi2Pk"]
+#     # )["Items"]
+
+#     # print(json.dumps(enterprise_users_response, indent=4, cls=DecimalEncoder))
+#     # print([x["email"] for x in enterprise_users_response])
+
+#     user = items_table.query_user_by_email("daniel@parsel.ai")
+#     e_users = items_table.query_enterprise_users(user["gsi2Pk"])["Items"]
+
+#     datasets = items_table.query_user_datasets(user["pk"])
+
+#     print([x["sk"] for x in datasets])
+#     print([x["email"] for x in e_users])
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from dynamodb_class import Items, DecimalEncoder
+import json
 
 """
 # Welcome to Streamlit!
@@ -18,28 +20,62 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 # TODO
 - Dynamodb pages per month, and per top users
+- Search for Dynamodb Enterprise user specific usage
 """
 
+items_table = Items()
 
-with st.echo(code_location="below"):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple("Point", "x y")
-    data = []
+def get_user():
+    user = items_table.query_user_by_email(st.session_state.email_input)
+    return json.loads(json.dumps(user, indent=4, cls=DecimalEncoder))
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+INIT_VALUE = "daniel@parsel.ai"
 
-    st.altair_chart(
-        alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color="#0068c9", opacity=0.5)
-        .encode(x="x:Q", y="y:Q")
-    )
+user_email = st.text_input(
+    "User email",
+    value=INIT_VALUE,
+    max_chars=None,
+    key="email_input",
+    type="default",
+    help=None,
+    autocomplete=None,
+    on_change=get_user,
+    args=None,
+    kwargs=None,
+    placeholder=None,
+    disabled=False,
+    label_visibility="visible",
+)
+
+output = get_user()
+st.write(output)
+
+
+def lets_write_yo(data):
+    st.write(data)
+
+
+# with st.echo(code_location="below"):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+#     Point = namedtuple("Point", "x y")
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(
+#         alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color="#0068c9", opacity=0.5)
+#         .encode(x="x:Q", y="y:Q")
+#     )
