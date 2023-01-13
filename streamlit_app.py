eb445109c829b6f3e11cffc996e9f6e66708af98
Daniel <daniel.vliegenthart@tellimer.com>
2023-01-13 15:37:18 +0000

    feat: 🎸 finish up initial features

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from copy import deepcopy
 
 """
 # Parsel.ai Internal Analytics
-This tool is a Work in Progress. For internal use only. Please contact Daniel V. on Slack if you have questions.
+For internal use only. Please contact Daniel V. on Slack if you have questions or requests.
 """
 
 items_table = Items()
@@ -76,6 +76,8 @@ else:
     is_enterprise = user.get("gsi2Pk") and "enterprise" in user.get("gsi2Pk")
     st.write(f"**Is Enterprise Account**: {is_enterprise == True}")
 
+    datasets = []
+
     if is_enterprise:
 
         enterprise = items_table.query_enterprise(user.get("gsi2Pk"))
@@ -136,109 +138,114 @@ else:
             )
 
         st.write("#### All Enterprise Account Datasets")
-        st.write("If empty, loading...")
 
-        datasets = []
         if is_enterprise:
             for user in enterprise_users:
                 datasets = datasets + items_table.query_user_datasets(user.get("pk"))
+    else:
+
+        st.write("#### User's Datasets")
+
+        datasets = items_table.query_user_datasets(user.get("pk"))
+
+    keys_to_extract = [
+        "fileName",
+        "status",
+        "outputs",
+        "audit",
+        "name",
+        "createdAt",
+        "fileSize",
+        "deleted",
+        "pageCount",
+        "numberFormat",
+        "updatedAt",
+        "progress",
+        "key",
+    ]
+
+    datasets = [
+        {key: item[key] for key in keys_to_extract if key in item} for item in datasets
+    ]
+
+    datasets = deepcopy(datasets)
+    # for dataset in datasets:
+    #     if type(dataset.get("outputs")) == list:
+    #         #     for output in dataset.get("outputs"):
+    #         #         if output.get("s3Key"):
+    #         #             dataset[
+    #         #                 f"output_{output.get('format')}"
+    #         #             ] = create_presigned_url(output.get("s3Key"))
+
+    #         #     dataset[f"input_document"] = create_presigned_url(dataset.get("key"))
+
+    #         del dataset["outputs"]
+    #     del dataset["key"]
+
+    df = pd.json_normalize(datasets, max_level=1)
+    df["createdAt"] = pd.to_datetime(df["createdAt"], unit="ms")
+    df["updatedAt"] = pd.to_datetime(df["updatedAt"], unit="ms")
+    df = df.sort_values(by=["createdAt"], ascending=False)
+    df = df.reset_index()
+    del df["index"]
+
+    column_to_move = df.pop("outputs")
+
+    # insert column with insert(location, column_name, column_value)
+    df.insert(21, "outputs", column_to_move)
+
+    st.write(df)
+
+    user_email = st.text_input(
+        "Row index to download",
+        value=0,
+        max_chars=None,
+        key="row_index",
+        type="default",
+        help=None,
+        autocomplete=None,
+        on_change=None,
+        args=None,
+        kwargs=None,
+        placeholder=None,
+        disabled=False,
+        label_visibility="visible",
+    )
+
+    def download_index():
+        index = st.session_state.row_index
+
+        if index.isdigit():
+            if int(index) > df.shape[0]:
+                st.session_state.warning = f"Row index too big, please select number between 1 and {df.shape[0]}"
+            else:
+                st.session_state.download_row = df.iloc[int(index)]
         else:
-            datasets = items_table.query_user_datasets(user.get("pk"))
+            st.session_state.warning = f"Not a valid row index, please select number between 1 and {df.shape[0]}"
 
-        keys_to_extract = [
-            "fileName",
-            "status",
-            "outputs",
-            "audit",
-            "name",
-            "createdAt",
-            "fileSize",
-            "deleted",
-            "pageCount",
-            "numberFormat",
-            "updatedAt",
-            "progress",
-            "key",
-        ]
+    st.button(
+        "Download outputs for row index",
+        key=None,
+        help=None,
+        on_click=download_index,
+        args=None,
+        kwargs=None,
+        type="secondary",
+        disabled=False,
+    )
 
-        datasets = [
-            {key: item[key] for key in keys_to_extract if key in item}
-            for item in datasets
-        ]
-        st.write(
-            "TODO: Dont generate presigned links, add download button, order by newest first by default"
-        )
+    if st.session_state.get("warning") is not None:
+        st.error(st.session_state.warning)
 
-        datasets = deepcopy(datasets)
-        # for dataset in datasets:
-        #     if type(dataset.get("outputs")) == list:
-        #         #     for output in dataset.get("outputs"):
-        #         #         if output.get("s3Key"):
-        #         #             dataset[
-        #         #                 f"output_{output.get('format')}"
-        #         #             ] = create_presigned_url(output.get("s3Key"))
+    if st.session_state.get("download_row") is not None:
+        outputs = st.session_state.get("download_row").get("outputs")
+        if outputs:
 
-        #         #     dataset[f"input_document"] = create_presigned_url(dataset.get("key"))
+            zip = [x for x in outputs if x["format"] == "ZIP"][0]
+            if zip.get("s3Key"):
+                st.write(
+                    f'[Download ZIP for row {st.session_state.row_index}]({create_presigned_url(zip.get("s3Key"))})'
+                )
 
-        #         del dataset["outputs"]
-        #     del dataset["key"]
-
-        df = pd.json_normalize(datasets, max_level=1)
-        df["createdAt"] = pd.to_datetime(df["createdAt"], unit="ms")
-        df["updatedAt"] = pd.to_datetime(df["updatedAt"], unit="ms")
-        df = df.sort_values(by=["createdAt"], ascending=False)
-        df = df.reset_index()
-        st.write(df)
-
-        user_email = st.text_input(
-            "Row index to download",
-            value=0,
-            max_chars=None,
-            key="row_index",
-            type="default",
-            help=None,
-            autocomplete=None,
-            on_change=None,
-            args=None,
-            kwargs=None,
-            placeholder=None,
-            disabled=False,
-            label_visibility="visible",
-        )
-
-        def download_index():
-            index = st.session_state.row_index
-
-            if index.isdigit():
-                if int(index) > df.shape[0]:
-                    st.warning(
-                        f"Row index too big, please select number between 1 and {df.shape[0]}"
-                    )
-                else:
-                    st.session_state.download_row = df.iloc[int(index)]
-            else:
-                st.warning("Not a valid row index")
-
-        st.button(
-            "Download outputs for row index",
-            key=None,
-            help=None,
-            on_click=download_index,
-            args=None,
-            kwargs=None,
-            type="secondary",
-            disabled=False,
-        )
-
-        if st.session_state.get("download_row") is not None:
-            outputs = st.session_state.get("download_row").get("outputs")
-            if outputs:
-
-                zip = [x for x in outputs if x["format"] == "ZIP"][0]
-                if zip.get("s3Key"):
-                    st.write(
-                        f'[Download ZIP for row {st.session_state.row_index}]({create_presigned_url(zip.get("s3Key"))})'
-                    )
-
-            else:
-                st.warning("This row index has no outputs available.")
+        else:
+            st.warning("This row index has no outputs available.")
